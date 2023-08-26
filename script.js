effa3ddb8deadfb606b3219b9661f52af529494f
“hashmi7917” <hmuqtadeer@gmail.com>
2023-08-26 23:02:00 +0530

    inital commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const itemForm = document.getElementById('item-form');
+const itemInput = document.getElementById('item-input');
+const itemList = document.getElementById('item-list');
+
+const addItem = (e) => {
+  e.preventDefault();
+  const newItem = itemInput.value;
+
+  // validate input
+  if (newItem === '') {
+    alert('Please add an item');
+    return;
+  }
+
+  // Create List item
+  const li = document.createElement('li');
+  li.appendChild(document.createTextNode(newItem));
+  const button = createButton('remove-item btn-link text-red');
+  li.appendChild(button);
+  itemList.appendChild(li);
+  itemInput.value = '';
+};
+
+const createButton = (classes) => {
+  const button = document.createElement('button');
+  button.className = classes;
+  const icon = createIcon('fa-solid fa-xmark');
+  button.appendChild(icon);
+  return button;
+};
+
+const createIcon = (classes) => {
+  const icon = document.createElement('i');
+  icon.className = classes;
+  return icon;
+};
+
+// Event Listerner
+itemForm.addEventListener('submit', addItem);
